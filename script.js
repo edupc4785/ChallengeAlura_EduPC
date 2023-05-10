3b994993b2cd3dc1a0daf739d42f33cb893774fe
Eduardo Peña Cirineo <edupc4785@gmail.com>
2023-05-10 18:25:03 -0500

    Implementacion del archivo js con sus funciones

---
+++ script.js
@@ -0,0 +1,55 @@
+const textArea = document.querySelector(".texto_receptor");
+const mensaje = document.querySelector(".texto_encriptado");
+
+/* La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat" */
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    console(mensaje)
+}
+
+function encriptar(stringEncriptado) {
+    let matrizCodigo = [["e", "enter"],["i", "imes"],["a", "ai"],["o", "ober"],["u", "ufat"]];
+    stringEncriptado = stringEncriptado.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptado.includes(matrizCodigo[i][0])) {
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        } 
+    }
+
+    return stringEncriptado;
+}
+
+function btnDesencriptar() {
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensaje.value = textoDesencriptado;
+}
+
+function desencriptar(stringDesencriptado) {
+    let matrizCodigo = [["e", "enter"],["i", "imes"],["a", "ai"],["o", "ober"],["u", "ufat"]];
+    stringDesencriptado = stringDesencriptado.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptado.includes(matrizCodigo[i][1])) {
+            stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        } 
+    }
+
+    return stringDesencriptado;
+}
+
+function copiar() {
+    mensaje.select();
+    navigator.clipboard.writeText(mensaje.value);
+}
+
+function eliminar() {
+    textArea.value = "";
+    mensaje.value = "";
+}
